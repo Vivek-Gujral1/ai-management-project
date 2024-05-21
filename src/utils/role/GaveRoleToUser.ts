5ef5af92f9f48fc2c17a1a0bdb41a97fd5becf0d
Vivek gujral <vivekgujral2345@gmail.com>
2024-05-21 12:51:21 +0530

    Giving Roles to user and updates in company created route

---
+++ src/utils/role/GaveRoleToUser.ts
@@ -0,0 +1,66 @@
+import prisma from "@/constants/prisma"
+import { ApiResponse } from "@/types/ApiResponse"
+
+interface functionParameters{
+    userId : string
+    Role   : string
+    companyId? : string 
+}
+
+export async function GaveRoleToUser({userId , companyId , Role} : functionParameters) : Promise<ApiResponse>{
+    try {
+        const user = await prisma.user.findFirst({
+            where : {
+                id : userId
+            }
+        })
+        if (!user) {
+            return {
+                message : "User not found" ,
+                success : false
+            }
+        }
+
+        const company = await prisma.company.findFirst({
+            where : {
+                id : companyId
+            }
+        }) 
+        if (!company) {
+            return {
+                message : "Company not found" ,
+                success : false
+            }
+        }
+
+        // gave role
+        await prisma.role.create({
+            data : {
+                Role ,
+                company : {
+                    connect : {
+                        id : company.id
+                    }
+                },
+                user  : {
+                    connect : {
+                        id : user.id
+                    }
+                } ,
+
+            }
+        })
+
+        return {
+            success : true ,
+            message : "Successfully gaves a role to user"
+        }
+
+    } catch (error) {
+        console.error("error while sending email" , error)
+        return {
+            success : false ,
+            message : "Failed to gave role"
+        }
+    }
+}
