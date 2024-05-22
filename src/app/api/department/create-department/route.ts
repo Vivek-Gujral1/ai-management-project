e944406112187edbc335f8f998b2caba27a94540
Vivek gujral <vivekgujral2345@gmail.com>
2024-05-22 13:54:27 +0530

    Department model and create department route complete

---
+++ src/app/api/department/create-department/route.ts
@@ -0,0 +1,107 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import prisma from "@/constants/prisma";
+import { SendCompanyVerificationEmail } from "@/utils/SendCompanyVerificationEmail";
+import { GaveRoleToUser } from "@/utils/role/GaveRoleToUser";
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const user = session.user;
+  try {
+    
+    const { name, headline }: { name: string; headline?: string } = await req.json();
+
+    const sokcetRoomName = `${user.id}_${name}`;
+
+    // creates socket room named  to ensure that with socket.io, only one room is created with the same name and no duplicate rooms are  created.
+    const exisitingDepartment = await prisma.company.findFirst({
+      where: {
+        sokcetRoomName,
+      },
+    });
+    if (exisitingDepartment) {
+      return Response.json(
+        {
+          success: false,
+          message: "User has already creates a department with this name",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { searchParams } = new URL(req.url);
+    const queryParam = {
+    companyId: searchParams.get("companyId"),
+   };
+
+    if (!queryParam.companyId) {
+        throw new Error("Invalid Query Parameter")
+    }
+
+   const comapny  = await prisma.company.findFirst({
+    where : {
+        id : queryParam.companyId
+    }
+   })
+
+   if (!comapny) {
+    return Response.json({
+        success : false,
+        message : "Comapny not found"
+    } , {status : 401})
+   }
+
+   const idLength = comapny?.id.length
+   console.log(`id length ${idLength}`);
+   
+   
+
+    const newDepartment = await prisma.department.create({
+        data : {
+            name ,
+            sokcetRoomName ,
+            headline : headline || "" ,
+            comapny : {
+                connect : {
+                    id : comapny?.id
+                }
+            }
+
+        }
+    })
+
+    const gaveRole = await GaveRoleToUser({Role :"Manager" , userId : user.id , depatmentId : newDepartment.id})
+
+    if (!gaveRole.success) {
+        return Response.json({
+            success : false,
+            message : gaveRole.message
+        } , {status : 500})
+    }
+    return Response.json({
+        success : true,
+        message : "Department Cretated Successfully"
+    } , {status : 200})
+
+  } catch (error) {
+    console.error("Company Creation error", error);
+
+    return Response.json(
+      {
+        success: false,
+        message: "Error while creating Comapany",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/utils/role/GaveRoleToUser.ts
+++ src/utils/role/GaveRoleToUser.ts
@@ -1,68 +1,88 @@
-import prisma from "@/constants/prisma"
-import { ApiResponse } from "@/types/ApiResponse"
+import prisma from "@/constants/prisma";
+import { ApiResponse } from "@/types/ApiResponse";
 
-import { userRole } from "@prisma/client"
+import { userRole } from "@prisma/client";
 
-interface functionParameters{
-    userId : string
-    Role   : userRole
-    companyId? : string 
+interface functionParameters {
+  userId: string;
+  Role: userRole;
+  companyId?: string;
+  depatmentId?: string;
 }
 
-export async function GaveRoleToUser({userId , companyId , Role} : functionParameters) : Promise<ApiResponse>{
-    try {
-        const user = await prisma.user.findFirst({
-            where : {
-                id : userId
-            }
-        })
-        if (!user) {
-            return {
-                message : "User not found" ,
-                success : false
-            }
-        }
-
-        const company = await prisma.company.findFirst({
-            where : {
-                id : companyId
-            }
-        }) 
-        if (!company) {
-            return {
-                message : "Company not found" ,
-                success : false
-            }
-        }
-
-        // gave role
-        await prisma.role.create({
-            data : {
-                Role   ,
-                company : {
-                    connect : {
-                        id : company.id
-                    }
-                },
-                user  : {
-                    connect : {
-                        id : user.id
-                    }
-                } ,
-
-            }
-        })
-
-        return {
-            success : true ,
-            message : "Successfully gaves a role to user"
-        }
-
-    } catch (error) {
-        console.error("error while sending email" , error)
-        return {
-            success : false ,
-            message : "Failed to gave role"
-        }
+export async function GaveRoleToUser({
+  userId,
+  companyId,
+  Role,
+  depatmentId,
+}: functionParameters): Promise<ApiResponse> {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      return {
+        message: "User not found",
+        success: false,
+      };
     }
+
+    if (companyId) {
+      // gave role
+      await prisma.role.create({
+        data: {
+          Role,
+          company: {
+            connect: {
+              id: companyId,
+            },
+          },
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      });
+
+      return {
+        success: true,
+        message: "Successfully gaves a role to user",
+      };
+    }
+    if (depatmentId) {
+      await prisma.role.create({
+        data: {
+          Role,
+          department: {
+            connect: {
+              id: depatmentId,
+            },
+          },
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      });
+
+      return {
+        success: true,
+        message: "Successfully gaves a role to user",
+      };
+    }
+    return {
+        success : false ,
+        message : "Invalid Parameters "
+    }
+  } catch (error) {
+    console.error("error while sending email", error);
+    return {
+      success: false,
+      message: "Failed to gave role",
+    };
+  }
 }
